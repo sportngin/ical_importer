b667f4d936c73ead3e4b0dd260d6d23b70ebc8a8
jnwaletzko <jnwaletzko@gmail.com>
2015-10-02 10:55:58 -0500

    Test if responds to tz_utc

--- lib/ical_importer/remote_event.rb
+++ lib/ical_importer/remote_event.rb
@@ -48,8 +48,8 @@ module IcalImporter
       event_method = event_method.to_sym
       raise ArgumentError, "Should be dtend or dtstart" unless [:dtstart, :dtend].include? event_method
       event_time = event.send event_method
-      if event_time.is_a? DateTime
-        (event_time.utc?) ? event_time.utc : event_time
+      if event_time.respond_to?(:tz_utc)
+        (event_time.tz_utc) ? event_time.utc : event_time
       else
         begin
           event_time.to_datetime
